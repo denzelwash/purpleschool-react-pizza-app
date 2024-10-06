f3370db5cb70b621935cbc235e1cc8e3984a5357
Denis Obolevich <denzelwash33@yandex.ru>
2024-10-06 12:08:34 +0300

    Перенес App в components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import Button from "../Button/Button";
+
+function App() {
+  return <Button className="primary">Привет</Button>;
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import "./assets/scss/main.scss";
+import App from "./components/App/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
