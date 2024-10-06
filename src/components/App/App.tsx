87637b02a64a9407da2838c9c450f36f85b7ddc9
Denis Obolevich <denzelwash33@yandex.ru>
2024-10-06 16:27:10 +0300

    css переменные и prettier all

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,11 @@
 import Button from "../Button/Button";
 
 function App() {
-  return <Button className="primary">Привет</Button>;
+  return (
+    <>
+      <Button className="primary">Привет</Button>sadasa
+    </>
+  );
 }
 
 export default App;
