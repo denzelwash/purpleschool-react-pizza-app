c120e34b7b2a417e5fdd221ffbcfbfae3ba25636
Denis Obolevich <denzelwash33@yandex.ru>
2024-10-17 20:42:35 +0300

    lazy импорт компонента для примера

--- src/router.tsx
+++ src/router.tsx
@@ -3,11 +3,13 @@ import Menu from "./pages/Menu/Menu";
 import Cart from "./pages/Cart/Cart";
 import Login from "./pages/Login/Login";
 import Error from "./pages/Error/Error";
-import Product from "./pages/Product/Product";
 import Sidebar from "./layouts/Sidebar/Sidebar";
 import axios from "axios";
 import { Product as ProductType } from "./types/product";
 import { API_URL } from "./const";
+import { lazy } from "react";
+
+const Product = lazy(() => import("./pages/Product/Product"));
 
 const router = createBrowserRouter([
   {
